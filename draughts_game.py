09934ddcaef759bd6bd487a0b77f7ba32c714006
Jose Esteban Garcia Grullon <josegg1897@gmail.com>
2019-09-06 14:27:58 -0400

    Fix typo and add draw matrix

--- draughts_game.py
+++ draughts_game.py
@@ -45,22 +45,22 @@ class Square:
 
 
 class Board:
-    
-    matrix = []
-    def __inti___(self):
+
+    def __init__(self,size_m = 8):
         self.matrix = []
+        self.size_m = size_m
 
     def generate_squares(self,player1,player2):
         i = True
-        for y in range(0,8):
+        for y in range(0,self.size_m):
             aux = []
-            for x in range(0,8):
-                if y < (8/2) -1 or y >= (8/2) + 1:                        
+            for x in range(0,self.size_m):
+                if y < (self.size_m/2) -1 or y >= (self.size_m/2) + 1:                        
                     if i:
                         aux.append(Square(Fore.WHITE))
                         i = False
                     else:
-                        if y > 8/2:
+                        if y > self.size_m/2:
                             aux.append(Square(Fore.RED,Piece(player1)))
                         else:
                             aux.append(Square(Fore.RED,Piece(player2)))
@@ -77,7 +77,18 @@ class Board:
             else:
                 i = True
             self.matrix.append(aux)
-    
+
+    def draw_matrix(self):
+        draw_b = ''
+        for row in self.matrix:
+            for col in row:
+                if col.is_piece_inside():
+                    draw_b += col.piece_color() + '© ' + reset
+                else:
+                    draw_b += col.color + '■ '+ reset
+            draw_b += '\n'
+        print(draw_b)
+
     def get_matrix(self):
         return self.matrix
 
@@ -87,24 +98,10 @@ player2 = Player('Carlos',Fore.YELLOW)
 reset = Style.RESET_ALL
 board = Board()
 board.generate_squares(player1,player2)
-b_matrix = board.get_matrix()
-b_matrix[3][0].assign_piece(b_matrix[2][1].piece)
-b_matrix[2][1].deallocate_piece()
+board.draw_matrix()
+# b_matrix[3][0].assign_piece(b_matrix[2][1].piece)
+# b_matrix[2][1].deallocate_piece()
 
-draw_b = ''
-for row in b_matrix:
-    for col in row:
-        if col.is_piece_inside():
-            draw_b += col.piece_color() + '@ ' + reset
-        else:
-            draw_b += col.color + '0 '+ reset
-    draw_b += '\n'
-#         if col.is_piece_inside():
-#             draw_b += str(col.piece.get_piece_color) + '@ ' + str(reset)
-#         else:
-#             draw_b += str(col.color) + '0 ' + str(reset)
-#     draw_b += '\n'
-print(draw_b)
 
 
 
