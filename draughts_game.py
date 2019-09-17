3fa5b325ad87a820b2585b975df76128e728df6e
Jose Esteban <josegg1897@gmail.com>
2019-09-17 02:51:26 -0400

    Add forced moves and eat after eat

--- draughts_game.py
+++ draughts_game.py
@@ -24,6 +24,9 @@ class Player:
 
     def get_amount_pieces(self):
         return len(self.pieces)
+    
+    def get_list_pieces(self):
+        return self.pieces
 
     def get_last_piece(self):
         return self.pieces[-1]
@@ -34,18 +37,19 @@ class Player:
     def get_player_num(self):
         return self.num
 
+    def get_player_dir(self):
+        if self.num == 2:
+            return +1
+        return -1
 
 class Piece:
     
-    def __init__(self,p_color,player_num):
-        self.player_num = player_num
+    def __init__(self,p_color):
         self.p_color = p_color
     
     def get_piece_color(self):
         return self.p_color
-    
-    def get_player_num(self):
-        return self.player_num
+
 
 
 
@@ -68,9 +72,6 @@ class Square:
     def piece_color(self):
         return self.piece.get_piece_color()
     
-    def get_piece_player_num(self):
-        return self.piece.get_player_num()
-
     def get_piece(self):
         return self.piece
 
@@ -85,17 +86,18 @@ class Board:
         i = True
         for y in range(0,self.size_m):
             aux = []
-            for x in range(0,self.size_m):
+            x = 0
+            while x < self.size_m:
                 if y < (self.size_m/2) -1 or y >= (self.size_m/2) + 1:                        
                     if i:
                         aux.append(Square(Fore.WHITE))
                         i = False
                     else:
                         if y > self.size_m/2:
-                            player1.add_piece(Piece(player1.get_player_color(),player1.get_player_num()))
+                            player1.add_piece(Piece(player1.get_player_color()))
                             aux.append(Square(Fore.RED,player1.get_last_piece()))
                         else:
-                            player2.add_piece(Piece(player2.get_player_color(),player2.get_player_num()))
+                            player2.add_piece(Piece(player2.get_player_color()))
                             aux.append(Square(Fore.RED,player2.get_last_piece()))
                         i = True
                 else:
@@ -104,7 +106,8 @@ class Board:
                         i = False
                     else:
                         aux.append(Square(Fore.RED))   
-                        i = True                    
+                        i = True
+                x += 1
             if i:
                 i = False
             else:
@@ -130,68 +133,119 @@ class Board:
             draw_b += '|'+str(con) +'\n'
         print(draw_b)
 
-    def move_piece(self,from_x,from_y,to_x,to_y,player_turn,opponent):
+    def move_piece(self,from_x,from_y,to_x,to_y,playing,opponent):
         
-        dif_y,dir_y = difference_between_and_direction(from_y,to_y)
-        dif_x,dir_x = difference_between_and_direction(from_x,to_x)
+        dif_y,dir_y = self.difference_between(from_y,to_y), self.direction(from_y,to_y)
+        dif_x,dir_x = self.difference_between(from_x,to_x), self.direction(from_x,to_x)
 
-        if dif_x == 0 or dif_y == 0 or not self.coordinates_is_in_range(from_x,from_y) or not self.coordinates_is_in_range(to_x,to_y):
-            return self.wrong(player_turn)
-        
-        if self.matrix[from_y][from_x].is_piece_inside():
-        
-            if self.matrix[from_y][from_x].get_piece_player_num() == player_turn:
-                if self.matrix[to_y][to_x].is_piece_inside():
-                    return self.wrong(player_turn)
-        
-                else:
-                    if dif_x > 2 or dif_y > 2 or dif_x != dif_y:
-                        return self.wrong(player_turn)
-                    
-                    elif dif_x == 2:
-                        if self.matrix[to_y - dir_y][to_x - dir_x].is_piece_inside():
-                            if self.matrix[to_y - dir_y][to_x - dir_x].get_piece_player_num() != player_turn:
-                                opponent.remove_piece(self.matrix[to_y - dir_y][to_x - dir_x].get_piece())
-                                self.matrix[to_y - dir_y][to_x - dir_x].deallocate_piece()
+        if not self.verify_moves(from_x,from_y,to_x,to_y,playing,dif_x,dif_y,dir_y):
+            return self.wrong(playing)
 
-                        else:
-                            return self.wrong(player_turn)
-                    
-                    elif dif_x == 1:
-                        if player_turn == 1:
-                            if from_y < to_y:
-                                return self.wrong(player_turn)
-                
-                        else:
-                            if from_y > to_y:
-                                return self.wrong(player_turn)
-                                
-                    self.matrix[to_y][to_x].assign_piece(self.matrix[from_y][from_x].piece)
-                    self.matrix[from_y][from_x].deallocate_piece()
-                    self.draw_matrix()
-        
-                    if player_turn == 1:
-                        player_turn = 2
-        
-                    else:
-                        player_turn = 1
-
-                    return player_turn
-        
-            else:
-                return self.wrong(player_turn,'Wrong piece')
-        
+        if dif_x == 2:
+            if not self.piece_can_eat(from_x,from_y,to_x,to_y,playing,dir_x,dir_y):
+                return self.wrong(playing)
+            opponent.remove_piece(self.matrix[to_y - dir_y][to_x - dir_x].get_piece())
+            self.matrix[to_y - dir_y][to_x - dir_x].deallocate_piece()
+            self.matrix[to_y][to_x].assign_piece(self.matrix[from_y][from_x].piece)
+            self.matrix[from_y][from_x].deallocate_piece()
+            if self.piece_can_eat_after_eat(to_x,to_y,playing) != []:
+                self.draw_matrix()
+                return playing
         else:
-            return self.wrong(player_turn)
+            self.matrix[to_y][to_x].assign_piece(self.matrix[from_y][from_x].piece)
+            self.matrix[from_y][from_x].deallocate_piece()        
+        self.draw_matrix()
+        return opponent        
 
     def wrong(self,pt,message = 'Wrong coordinates'):
         self.draw_matrix()
         print(message)
         return pt
     
-    def verify_forced_movements(self):
-        pass
+    def verify_moves(self,from_x,from_y,to_x,to_y,playing,dif_x,dif_y,dir_y):
         
+        if dif_x == 0 or dif_y == 0 or not self.coordinates_is_in_range(from_x,from_y) or not self.coordinates_is_in_range(to_x,to_y):
+            return False
+        
+        if not self.matrix[from_y][from_x].is_piece_inside():
+            return False
+
+        if self.matrix[from_y][from_x].get_piece() not in playing.get_list_pieces():
+            return False
+
+        if self.matrix[to_y][to_x].is_piece_inside():
+            return False
+        
+        if dif_x > 2 or dif_y > 2 or dif_x != dif_y:
+            return False
+        
+        if dir_y != playing.get_player_dir():
+            return False
+        return True
+
+
+    def verify_forced_movements(self,playing):
+        forced = []
+        change_y = 2 if playing.get_player_dir() == 1 else -2
+        index_row = 0
+        while index_row < self.size_m:
+            
+            index_col = 0
+            while index_col < len(self.matrix[index_row]):
+            
+                to_x = index_col + 2
+                to_y = index_row + change_y
+                dif_x,dif_y,dir_x,dir_y = self.difference_between_and_direction(index_col,index_row,to_x,to_y)
+            
+                if self.verify_moves(index_col,index_row,to_x,to_y,playing,dif_x,dif_y,dir_y):
+            
+                    if self.piece_can_eat(index_col,index_row,to_x,to_y,playing,dir_x,dir_y):
+                        forced.append([index_col,index_row,to_x,to_y])
+            
+                to_x = index_col - 2
+                dif_x, dir_x = self.difference_between(index_col,to_x),self.direction(index_col,to_x)
+            
+                if self.verify_moves(index_col,index_row,to_x,to_y,playing,dif_x,dif_y,dir_y):
+            
+                    if self.piece_can_eat(index_col,index_row,to_x,to_y,playing,dir_x,dir_y):
+            
+                        forced.append([index_col,index_row,to_x,to_y])
+                index_col += 1
+            index_row += 1
+        return forced
+    
+    def piece_can_eat(self,from_x,from_y,to_x,to_y,playing,dir_x,dir_y):
+        if self.matrix[to_y - dir_y][to_x - dir_x].is_piece_inside():
+            if self.matrix[to_y - dir_y][to_x - dir_x].get_piece() not in playing.get_list_pieces():
+                return True
+        return False
+    def piece_can_eat_after_eat(self,from_x,from_y,playing):
+        moves = []
+        to_x = from_x +2
+        to_y = from_y +2
+        dir_x,dir_y = self.direction(from_x,to_x),self.direction(from_y,to_y)
+        if self.verify_moves(from_x,from_y,to_x,to_y,playing,2,2,dir_y):
+            if self.piece_can_eat(from_x,from_y,to_x,to_y,playing,dir_x,dir_y):
+                moves.append([from_x,from_y,to_x,to_y])
+        to_x = from_x +2
+        to_y = from_y -2
+        dir_x,dir_y = self.direction(from_x,to_x),self.direction(from_y,to_y)
+        if self.verify_moves(from_x,from_y,to_x,to_y,playing,2,2,dir_y):
+            if self.piece_can_eat(from_x,from_y,to_x,to_y,playing,dir_x,dir_y):
+                moves.append([from_x,from_y,to_x,to_y])
+        to_x = from_x -2
+        to_y = from_y +2
+        dir_x,dir_y = self.direction(from_x,to_x),self.direction(from_y,to_y)
+        if self.verify_moves(from_x,from_y,to_x,to_y,playing,2,2,dir_y):
+            if self.piece_can_eat(from_x,from_y,to_x,to_y,playing,dir_x,dir_y):
+                moves.append([from_x,from_y,to_x,to_y])
+        to_x = from_x -2
+        to_y = from_y -2
+        dir_x,dir_y = self.direction(from_x,to_x),self.direction(from_y,to_y)
+        if self.verify_moves(from_x,from_y,to_x,to_y,playing,2,2,dir_y):
+            if self.piece_can_eat(from_x,from_y,to_x,to_y,playing,dir_x,dir_y):
+                moves.append([from_x,from_y,to_x,to_y])
+        return moves
     def get_matrix(self):
         return self.matrix
     
@@ -200,13 +254,20 @@ class Board:
             return True
         return False
 
+    def difference_between(self,a,b):
+        if a > b:
+            return a - b
+        return b - a
+    
+    def direction(self,a,b):
+        if a > b:
+            return -1
+        return +1
 
-def difference_between_and_direction(a,b):
-    if a > b:
-        return a - b,-1
-    else:
-        return b - a,+1
-
+    def difference_between_and_direction(self,from_x,from_y,to_x,to_y):
+        dif_x, dif_y =self.difference_between(from_x,to_x), self.difference_between(from_y,to_y)
+        dir_x, dir_y = self.direction(from_x,to_x), self.direction(from_y,to_y)
+        return dif_x, dif_y, dir_x, dir_y
 
 while True:
     name_player1 = input('Write name of player number 1: ')
@@ -216,13 +277,12 @@ while True:
     board = Board(8)
     board.generate_squares(player1,player2)
     board.draw_matrix()
-    player_turn = player1.get_player_num()
+    playing = player1
     while player1.get_amount_pieces() > 0 or player2.get_amount_pieces() > 0:
-        if player_turn == player1.get_player_num():
-            print(player1.get_name_player() + ' Turn ' +str(player1.get_amount_pieces()) + ' Pieces' )
+        print(playing.get_name_player() + ' Turn ' +str(playing.get_amount_pieces()) + ' Pieces' )
+        if playing == player1:
             opponent = player2
         else:
-            print(player2.get_name_player() + ' Turn ' +str(player2.get_amount_pieces()) + ' Pieces' )
             opponent = player1
         print('Enter the coordinates (x, y) of the tab to move')
         from_x = int(input('x: ')) - 1
@@ -232,7 +292,15 @@ while True:
         to_y = int(input('y: ')) - 1
         if from_x == -1 and from_y == -1:
             break
-        player_turn =board.move_piece(from_x,from_y,to_x,to_y,player_turn,opponent)
+        forced_movements = board.verify_forced_movements(playing)
+        if forced_movements != []:
+            if [from_x,from_y,to_x,to_y] in forced_movements:
+                playing =board.move_piece(from_x,from_y,to_x,to_y,playing,opponent)
+            else:
+                board.draw_matrix()
+                print("Mandatory movements")
+        else:
+            playing = board.move_piece(from_x,from_y,to_x,to_y,playing,opponent)
     salir = input('Write \'s\' if you want to end the game: ')
     if salir == 's' or salir == 'S':
         break
